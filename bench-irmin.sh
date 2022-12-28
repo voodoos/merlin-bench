91a87e96de7935fd1772a929b89e9b191b38284e
Ulysse Gérard <thevoodoos@gmail.com>
2022-12-28 18:23:03 +0100

    More tweaks

--- bench-irmin.sh
+++ bench-irmin.sh
@@ -1,3 +1,3 @@
-ocamlmerlin_tester --project=irmin --queries=locate --repeats=1 --sample-size=1 --data=data
+opam exec -- ocamlmerlin_tester --project=irmin --queries=locate --repeats=1 --sample-size=1 --data=data
 sed 's/,"timing":{.*0}//g' data/merlin_reponses.json >irmin-responses.json.new
 diff irmin-responses.json irmin-responses.json.new
